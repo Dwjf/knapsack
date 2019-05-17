2bb61853d57f4895b2b9a80a4968cc608f73a944
Daniel <wongjerngfoong@gmail.com>
2019-05-17 22:02:51 +0800

    Update knapsack1.py

--- knapsack1.py
+++ knapsack1.py
@@ -12,7 +12,6 @@ cost = [
 ]
 
 constraint = 200000
-'''add constraint on cost value.'''
 
 def generate_chrome(num_gene=15):
     '''generate one random chromosome values '''
@@ -23,7 +22,7 @@ def generate_chrome(num_gene=15):
     return chromosome
 
 def score_weight(chromosome,cost):
-    '''to score one chromosome.
+    '''to score one chromosome
 
     chromosome: list
     '''
@@ -33,39 +32,94 @@ def score_weight(chromosome,cost):
         weightlist.append(score)
     return sum(weightlist)
 
+import datetime
+
+startTime = datetime.datetime.now()
+
+def display(guess):
+        timeDiff = datetime.datetime.now() - startTime
+        #fitness = get_fitness(guess)
+        fitness = score_value(guess,value)
+        weight = score_weight(guess,cost)
+        print("{0}\t{1}\t{2}\t{3}".format(guess, fitness,weight, str(timeDiff)))
+
+
 def score_value(chromosome,value):
     '''to score one chromosome
 
     chromosome: list
     '''
     valuelist=[]
+    #print(chromosome)
     for i in range(len(chromosome)):
         score=value[i]*chromosome[i]
         valuelist.append(score)
     return sum(valuelist)
 
+#print(generate_chrome())
 def is_feasible(weightscore,constraint):
-    '''predicate on feasibility, that is values less than constraint'''
     if weightscore < constraint:
         return True
     return False
-        
-def main(n):
+
+
+print(score_weight(generate_chrome(),cost))
+'''
+def execute():
+    '''
+'''generate and return feasible chromosome binary, weight, and
+    value score, and assign value to max
+    if is better score than previous
+'''
+'''
+    current_binary=[]
+    max_value = 0
+    c = generate_chrome()
+    print("c",c)
+    w=score_weight(c,cost)
+    print("Weight score",w)
+    print("Constraint",constraint)
+    v=score_value(c,value)
+    print("Value score",v)
+    if is_feasible(w,constraint):
+        print("Feasible")
+        current_binary = c
+        max_value = v
+        print(
+        "Max chromosome",current_binary,
+        "value is:",max_value
+        )
+    else:
+        #return False
+        print("Not Feasible solution")
+     '''   
+def iteration(n):
     "Iterate and save best result"
     optimal=0
     best_binary=[]
     for i in range(n):
+       # print(i)
         c=generate_chrome()
+        #print("Chromosome values",c)
         v = score_value(c,value)
+        #print("V values:",v)
         w = score_weight(c,cost)
+       # print("Weight values",w)
         if is_feasible(w,constraint):
             if v>optimal:
                 optimal=v
                 best_binary=c
                 best_weight = w
-                print("Feasible Value",optimal,"at iteration",i,
-                "Optimal binary",best_binary,"Weight:",best_weight)
-    return optimal,"Best binary is",best_binary,"at weight of:",best_weight
+                #print("Feasible Value",optimal,"at iteration",i,
+                #"Optimal binary",best_binary,"Weight:",best_weight)
+                display(best_binary)
+    #return optimal,"Best binary is",best_binary,"at weight of:",best_weight
+    #return display(best_binary)
 
-print("Optimal value of:",main(30000))
+iteration(40000)
+#execute()
+'''
+c=generate_chrome()
+score_value(c,value)
 
+'''
